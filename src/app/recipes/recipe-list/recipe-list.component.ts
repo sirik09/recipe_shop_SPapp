a114f08e175f52b660994f59a1b6225c642982a1
sngassa <stephane.ngassa@altran.it>
2019-02-07 18:31:47 +0100

    create a Recipe model. the next step is to iterate over an array of recipes an then print array contents dynamically on html page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('a test recipe',
+    'this is simply a test',
+    'https://cdn-image.foodandwine.com/sites/default/files/HD-201204-r-vinegar-braised-chicken-with-leeks-and-peas.jpg')
+];
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -7,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
